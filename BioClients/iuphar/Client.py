19ac58a87ee595fb30dbbbd34dd6257a6532448b
Jeremy Yang <jeremyjyang@gmail.com>
2020-03-24 16:31:53 -0600

    Ok.

---
+++ BioClients/iuphar/Client.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+##############################################################################
+### See http://www.guidetopharmacology.org/webServices.jsp
+##############################################################################
+import sys,os,re,argparse,time,logging,urllib.parse,json
+#
+from .. import iuphar
+#
+API_HOST='www.guidetopharmacology.org'
+API_BASE_PATH='/services'
+#
+#############################################################################
+if __name__=='__main__':
+  SEARCH_TYPE='substructure'; 
+  epilog='''\
+operations:
+  list_targets: list targets (selected by type, db, accessionId);
+  list_target_families: list target families ;
+  get_target: get target (requires ID);
+  get_target_interactions: get target interactions (requires ID);
+  get_target_dblinks: get target dblinks (requires ID);
+  get_target_substrates: get target substrates (requires ID);
+  get_target_function: get target function (requires ID);
+  get_target_products: get target products (requires ID);
+  get_target_gpinfo: get target gene protein info (requires ID);
+  get_ligand_synonyms: get ligand synonyms (requires ID);
+  list_ligands: list ligands (selected by type, db, accessionId);
+  get_ligand: get ligand (requires ID);
+  get_ligand_interactions: get ligand interactions (requires ID);
+  get_ligand_dblinks: get ligand dblinks (requires ID);
+  get_ligand_structure: get ligand structure (requires ID);
+  get_target_synonyms: get target synonyms (requires ID);
+  search_ligand: structure search (requires smiles query);
+'''
+
+  parser = argparse.ArgumentParser(description='IUPHAR/GuideToPharmacolog API client', epilog=epilog)
+  ops=[ "list_targets", "list_target_families", "get_target", "get_target_interactions", "get_target_dblinks", "get_target_substrates", "get_target_function", "get_target_products", "get_target_gpinfo", "get_ligand_synonyms", "list_ligands", "get_ligand", "get_ligand_interactions", "get_ligand_dblinks", "get_ligand_structure", "get_target_synonyms", "search_ligand" ]
+  parser.add_argument("op", choices=ops, help='operation')
+  parser.add_argument("--i", dest="ifile", help="input file of IDs (targetId|ligandId|accessionId)")
+  parser.add_argument("--ids", help="ID list (comma-separated)(targetId|ligandId|accessionId)")
+  parser.add_argument("--o", dest="ofile", help="output file (TSV)")
+  parser.add_argument("--tgt_type", help="GPCR|NHR|LGIC|VGIC|OtherIC|Enzyme|CatalyticReceptor|Transporter|OtherProtein|AccessoryProtein")
+  parser.add_argument("--lig_type", help="Synthetic organic|Metabolite|Natural product|Endogenous peptide|Peptide|Antibody|Inorganic|Approved|Withdrawn|Labelled|INN")
+  parser.add_argument("--itr_type", help="Activator|Agonist|Allosteric modulator|Antagonist|Antibody|Channel blocker|Gating inhibitor|Inhibitor|Subunit-specific")
+  parser.add_argument("--aff", type=float, help="affinity minimun")
+  parser.add_argument("--aff_type", help="pA2|pEC50|pIC50|pKB|pKd|pKi")
+  parser.add_argument("--smiles", help="structure search query")
+  parser.add_argument("--search_type", help="substructure|exact|similarity")
+  parser.add_argument("--species", help="Human|Mouse|Rat|etc.")
+  parser.add_argument("--db", help="external accessionId database ChEMBL|EntrezGene|HGNC|PubChemCID|UniProt|etc.")
+  parser.add_argument("--api_host", default=API_HOST)
+  parser.add_argument("--api_base_path", default=API_BASE_PATH)
+  parser.add_argument("-v", "--verbose", default=0, action="count")
+
+  args = parser.parse_args()
+
+  logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
+
+  BASE_URL = 'https://'+args.api_host+args.api_base_path
+
+  if args.ofile:
+    fout = open(args.ofile, "w+")
+  else:
+    fout = sys.stdout
+
+  ids=[]
+  if args.ifile:
+    fin = open(args.ifile)
+    while True:
+      line = fin.readline()
+      if not line: break
+      ids.append(line.strip())
+  elif args.ids:
+    ids = re.split(r'[,\s]+', args.ids.strip())
+
+  t0=time.time()
+
+  if args.op == "list_targets":
+    iuphar.Utils.ListTargets(BASE_URL, args.tgt_type, args.db, ids, fout)
+
+  elif args.op == "list_target_families":
+    iuphar.Utils.ListTargetFamilies(BASE_URL, args.tgt_type, fout)
+
+  elif args.op == "get_target":
+    iuphar.Utils.GetTarget(BASE_URL, ids, fout)
+
+  elif args.op == "get_target_interactions":
+    iuphar.Utils.GetInteractions(BASE_URL, 'targets', ids, args.species, args.itr_type, args.aff_type, args.aff, fout)
+
+  elif args.op == "get_target_dblinks":
+    iuphar.Utils.GetDblinks(BASE_URL, 'targets', ids, args.species, args.db, fout)
+
+  elif args.op == "get_target_substrates":
+    iuphar.Utils.GetTargetSubstrates(BASE_URL, ids, args.species, fout)
+
+  elif args.op == "get_target_products":
+    iuphar.Utils.GetTargetProducts(BASE_URL, ids, args.species, fout)
+
+  elif args.op == "get_target_function":
+    iuphar.Utils.GetTargetFunction(BASE_URL, ids, args.species, fout)
+
+  elif args.op == "get_target_gpinfo":
+    iuphar.Utils.GetTargetGeneProteinInfo(BASE_URL, ids, fout)
+
+  elif args.op == "get_target_synonyms":
+    iuphar.Utils.GetSynonyms(BASE_URL, 'targets', ids, fout)
+
+  elif args.op == "list_ligands":
+    iuphar.Utils.ListLigands(BASE_URL, args.lig_type, args.db, ids, fout)
+
+  elif args.op == "get_ligand":
+    iuphar.Utils.GetLigand(BASE_URL, ids, fout)
+
+  elif args.op == "get_ligand_interactions":
+    iuphar.Utils.GetInteractions(BASE_URL, 'ligands', ids, args.species, args.itr_type, args.aff_type, args.aff, fout)
+
+  elif args.op == "get_ligand_dblinks":
+    iuphar.Utils.GetDblinks(BASE_URL, 'ligands', ids, fout)
+
+  elif args.op == "get_ligand_structure":
+    iuphar.Utils.GetLigandStructure(BASE_URL, ids, fout)
+
+  elif args.op == "get_ligand_synonyms":
+    iuphar.Utils.GetSynonyms(BASE_URL, 'ligands', ids, fout)
+
+  elif args.op == "search_ligand":
+    if not (args.smiles and args.search_type):
+      parser.error('--smiles and --search_type required.'+usage)
+    iuphar.Utils.SearchLigand(BASE_URL, args.smiles, args.search_type, fout)
+
+  else:
+    parser.error('Invalid operation: %s'%args.op)
+      
+  logging.info('Elapsed time: %s'%(time.strftime('%Hh:%Mm:%Ss',time.gmtime(time.time()-t0))))
+
---
+++ BioClients/iuphar/Utils.py
@@ -0,0 +1,415 @@
+#!/usr/bin/env python3
+##############################################################################
+### See http://www.guidetopharmacology.org/webServices.jsp
+##############################################################################
+import sys,os,re,time,logging,urllib.parse,json
+#
+from ..util import rest_utils
+#
+INT_TAGS=[
+	"interactionId",
+	"targetId",
+	"ligandAsTargetId",
+	"targetSpecies",
+	"primaryTarget",
+	"targetBindingSite",
+	"ligandId",
+	"ligandContext",
+	"endogenous",
+	"type",
+	"action",
+	"actionComment",
+	"selectivity",
+	"concentrationRange",
+	"affinity",
+	"affinityType",
+	"originalAffinity",
+	"originalAffinityType",
+	"originalAffinityRelation",
+	"assayDescription",
+	"assayConditions",
+	"useDependent",
+	"voltageDependent",
+	"voltage",
+	"physiologicalVoltage"
+]
+INT_REF_TAGS=[
+	"referenceId",
+	"pmid",
+	"type",
+	"title",
+	"year",
+	"volume",
+	"issue",
+	"pages",
+	"articleTitle",
+	"authors",
+	"editors",
+	"publisher",
+	"publisherAddress",
+	"pubStatus",
+	"website",
+	"doi",
+	"url",
+	"isbn",
+	"patentNumber",
+	"assignee",
+	"accessDate",
+	"modifyDate",
+	"priorityDate",
+	"publicationDate"
+]
+#############################################################################
+def ListTargets(base_url, tgt_type, db, ids, fout):
+  n_tgt=0; n_out=0; n_err=0;
+  tags=None;
+  url=base_url+'/targets?'
+  if tgt_type: url+=('&type=%s'%urllib.parse.quote(tgt_type))
+  if db: url+=('&database=%s'%(db))
+  for id_this in ids:
+    url_this = url+('&accession=%s'%(urllib.parse.quote(id_this)))
+    rval=rest_utils.GetURL(url_this, parse_json=True)
+    tgts=rval
+    for tgt in tgts:
+      n_tgt+=1
+      if n_tgt==1 or not tags:
+        tags=sorted(tgt.keys())
+        fout.write(('\t'.join(tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((tgt[tag]) if tag in tgt else '')
+      fout.write(('\t'.join(vals))+'\n')
+      n_out+=1
+    
+  #logging.info(json.dumps(rval, indent=2))
+  logging.info('n_tgt: %d'%n_tgt)
+  logging.info('n_out: %d'%n_out)
+
+############################# ################################################
+def ListTargetFamilies(base_url,  tgt_type, fout):
+  n_fam=0; n_out=0; n_err=0;
+  tags=None;
+  url=base_url+'/targets/families'
+  if tgt_type: url+=('?type=%s'%urllib.parse.quote(tgt_type))
+  rval=rest_utils.GetURL(url, parse_json=True)
+  fams=rval
+  for fam in fams:
+    n_fam+=1
+    if n_fam==1 or not tags:
+      tags=sorted(fam.keys())
+      fout.write(('\t'.join(tags))+'\n')
+    vals=[]
+    for tag in tags:
+      vals.append((fam[tag]) if tag in fam else '')
+    fout.write(('\t'.join(vals))+'\n')
+    n_out+=1
+    
+  #logging.info(json.dumps(rval, indent=2))
+  logging.info('n_fam: %d'%n_fam)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetTarget(base_url, ids, fout):
+  n_qry=0; n_tgt=0; n_out=0; n_err=0;
+  tags=None;
+  for tid in ids:
+    n_qry+=1
+    url=base_url+'/targets/%s'%tid
+    rval=rest_utils.GetURL(url, parse_json=True)
+    tgt=rval
+    n_tgt+=1
+    if n_tgt==1 or not tags:
+      tags=sorted(tgt.keys())
+      fout.write(('\t'.join(tags))+'\n')
+    vals=[]
+    for tag in tags:
+      vals.append((tgt[tag]) if tag in tgt else '')
+    fout.write(('\t'.join(vals))+'\n')
+    n_out+=1
+
+  logging.info('n_tgt: %d'%n_tgt)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetTargetSubstrates(base_url, ids, species, fout):
+  n_qry=0; n_tsb=0; n_out=0; n_err=0;
+  tags=None;
+  for id_query in ids:
+    n_qry+=1
+    url=base_url+('/targets/%s/substrates'%id_query)
+    if species: url+=('?species=%s'%urllib.parse.quote(species))
+    rval=rest_utils.GetURL(url, parse_json=True)
+    tsbs=rval
+    for tsb in tsbs:
+      n_tsb+=1
+      if n_tsb==1 or not tags:
+        tags=sorted(tsb.keys())
+        fout.write(('\t'.join(tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((tsb[tag]) if tag in tsb else '')
+      fout.write(('\t'.join(vals))+'\n')
+      n_out+=1
+
+  logging.info('n_tsb: %d'%n_tsb)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetTargetProducts(base_url, ids, species, fout):
+  n_qry=0; n_prd=0; n_out=0; n_err=0;
+  tags=None;
+  for id_query in ids:
+    n_qry+=1
+    url=base_url+('/targets/%s/products'%id_query)
+    if species: url+=('?species=%s'%urllib.parse.quote(species))
+    rval=rest_utils.GetURL(url, parse_json=True)
+    prds=rval
+    for prd in prds:
+      n_prd+=1
+      if n_prd==1 or not tags:
+        tags=sorted(prd.keys())
+        fout.write(('\t'.join(tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((prd[tag]) if tag in prd else '')
+      fout.write(('\t'.join(vals))+'\n')
+      n_out+=1
+
+  logging.info('n_prd: %d'%n_prd)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetTargetFunction(base_url, ids, species, fout):
+  n_qry=0; n_fnc=0; n_out=0; n_err=0;
+  tags=None;
+  for id_query in ids:
+    n_qry+=1
+    url=base_url+('/targets/%s/function'%id_query)
+    if species: url+=('?species=%s'%urllib.parse.quote(species))
+    rval=rest_utils.GetURL(url, parse_json=True)
+    fncs=rval
+    for fnc in fncs:
+      n_fnc+=1
+      if n_fnc==1 or not tags:
+        tags=sorted(fnc.keys())
+        fout.write(('\t'.join(tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((fnc[tag]) if tag in fnc else '')
+      fout.write(('\t'.join(vals))+'\n')
+      n_out+=1
+
+  logging.info('n_fnc: %d'%n_fnc)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetTargetGeneProteinInfo(base_url, ids, fout):
+  n_qry=0; n_gpi=0; n_out=0; n_err=0;
+  tags=None;
+  for tid in ids:
+    n_qry+=1
+    url=base_url+'/targets/%s/geneProteinInformation'%tid
+    rval=rest_utils.GetURL(url, parse_json=True)
+    gpis=rval
+    for gpi in gpis:
+      n_gpi+=1
+      if n_gpi==1 or not tags:
+        tags=sorted(gpi.keys())
+        fout.write(('\t'.join(tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((gpi[tag]) if tag in gpi else '')
+      fout.write(('\t'.join(vals))+'\n')
+      n_out+=1
+
+  logging.info('n_gpi: %d'%n_gpi)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def ListLigands(base_url, lig_type, db, ids, fout):
+  n_lig=0; n_out=0; n_err=0;
+  tags=None;
+  url=base_url+'/ligands?'
+  if lig_type: url+=('&type=%s'%urllib.parse.quote(lig_type))
+  if db: url+=('&database=%s'%(db))
+  for id_this in ids:
+    url_this = url+('&database=%s&accession=%s'%(db, urllib.parse.quote(id_this)))
+    rval = rest_utils.GetURL(url_this, parse_json=True)
+    ligs=rval
+    for lig in ligs:
+      n_lig+=1
+      if n_lig==1 or not tags:
+        tags=sorted(lig.keys())
+        fout.write(('\t'.join(tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((lig[tag]) if tag in lig else '')
+      fout.write(('\t'.join(vals))+'\n')
+      n_out+=1
+    
+  logging.info('n_lig: %d'%n_lig)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetLigand(base_url, ids, fout):
+  n_qry=0; n_lig=0; n_out=0; n_err=0;
+  tags=None;
+  for lid in ids:
+    n_qry+=1
+    url=base_url+'/ligands/%s'%lid
+    rval=rest_utils.GetURL(url, parse_json=True)
+    lig=rval
+    n_lig+=1
+    if n_lig==1 or not tags:
+      tags=sorted(lig.keys())
+      fout.write(('\t'.join(tags))+'\n')
+    vals=[]
+    for tag in tags:
+      vals.append((lig[tag]) if tag in lig else '')
+    fout.write(('\t'.join(vals))+'\n')
+    n_out+=1
+
+  logging.info('n_lig: %d'%n_lig)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetLigandStructure(base_url, ids, fout):
+  '''Smiles backslashes may cause problems.'''
+  n_qry=0; n_lig=0; n_out=0; n_err=0;
+  tags=None;
+  for lid in ids:
+    n_qry+=1
+    url=base_url+'/ligands/%s/structure'%lid
+    rval=rest_utils.GetURL(url, parse_json=True)
+    lig=rval
+    if not lig:
+      logging.error('Not found: %s'%(url))
+      n_err+=1
+      continue
+    n_lig+=1
+    if n_lig==1 or not tags:
+      tags=sorted(lig.keys())
+      fout.write(('\t'.join(tags))+'\n')
+    vals=[]
+    for tag in tags:
+      vals.append((lig[tag]) if tag in lig else '')
+    fout.write(('\t'.join(vals))+'\n')
+    n_out+=1
+
+  logging.info('n_lig: %d'%n_lig)
+  logging.info('n_out: %d'%n_out)
+  logging.info('n_err: %d'%n_err)
+
+#############################################################################
+def GetInteractions(base_url, resource, ids, species, itr_type, aff_type, aff, fout):
+  '''Include first reference only.'''
+  n_qry=0; n_itr=0; n_out=0; n_err=0;
+  n_dpt=0;
+  for id_query in ids:
+    n_qry+=1
+    url=base_url+('/%s/%s/interactions?'%(resource, id_query))
+    if species: url+=('&species=%s'%urllib.parse.quote(species))
+    if itr_type: url+=('&type=%s'%urllib.parse.quote(itr_type))
+    if aff_type: url+=('&affinityType=%s'%urllib.parse.quote(aff_type))
+    if aff: url+=('&affinity=%.2f'%aff)
+    rval=rest_utils.GetURL(url, parse_json=True)
+    itrs=rval
+    if not itrs or type(itrs) not in (list, tuple):
+      logging.error('Not found: %s'%(url))
+      n_err+=1
+      continue
+    for itr in itrs:
+      n_itr+=1
+      dpts = itr['dataPoints'] if 'dataPoints' in itr else []
+      for dpt in dpts:
+        n_dpt+=1
+        if n_dpt==1:
+          fout.write(('\t'.join(INT_TAGS+INT_REF_TAGS))+'\n')
+        vals=[]
+        for tag in INT_TAGS:
+          vals.append((dpt[tag]) if tag in dpt else '')
+        refs = dpt['refs'] if 'refs' in dpt else []
+        if refs:
+          for tag in INT_REF_TAGS:
+            vals.append((refs[0][tag]) if tag in refs[0] else '')
+        else:
+          vals.extend(['' for tag in INT_REF_TAGS])
+        fout.write(('\t'.join(vals))+'\n')
+        n_out+=1
+
+  logging.info('n_itr: %d'%n_itr)
+  logging.info('n_dpt: %d'%n_dpt)
+  logging.info('n_out: %d'%n_out)
+  logging.info('n_err: %d'%n_err)
+
+#############################################################################
+def SearchLigand(base_url, smi, search_type, fout):
+  n_lig=0; n_out=0; n_err=0;
+  tags=None;
+  url=base_url+('/ligands/%s?smiles=%s'%(search_type, urllib.parse.quote(smi)))
+  rval=rest_utils.GetURL(url, parse_json=True)
+  ligs=rval
+  for lig in ligs:
+    n_lig+=1
+    if n_lig==1 or not tags:
+      tags=sorted(lig.keys())
+      fout.write(('\t'.join(tags))+'\n')
+    vals=[]
+    for tag in tags:
+      vals.append((lig[tag]) if tag in lig else '')
+    fout.write(('\t'.join(vals))+'\n')
+    n_out+=1
+    
+  logging.info('n_lig: %d'%n_lig)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetSynonyms(base_url, resource, ids, fout):
+  '''resource = targets|ligands'''
+  n_qry=0; n_syn=0; n_out=0; n_err=0;
+  tags=None;
+  for id_query in ids:
+    n_qry+=1
+    url=base_url+'/%s/%s/synonyms'%(resource, id_query)
+    rval=rest_utils.GetURL(url, parse_json=True)
+    syns=rval
+    for syn in syns:
+      n_syn+=1
+      if n_syn==1 or not tags:
+        tags=sorted(syn.keys())
+        fout.write(('\t'.join(['%s_id'%resource]+tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((syn[tag]) if tag in syn else '')
+      fout.write(('\t'.join([id_query]+vals))+'\n')
+      n_out+=1
+
+  logging.info('n_syn: %d'%n_syn)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
+def GetDblinks(base_url, resource, ids, species, db, fout):
+  n_qry=0; n_dbl=0; n_out=0; n_err=0;
+  tags=None;
+  for id_query in ids:
+    n_qry+=1
+    url=base_url+'/%s/%s/databaseLinks?'%(resource, id_query)
+    if species: url+=('&species=%s'%urllib.parse.quote(species))
+    if db: url+=('&database=%s'%urllib.parse.quote(db))
+    rval=rest_utils.GetURL(url, parse_json=True)
+    dbls=rval
+    for dbl in dbls:
+      n_dbl+=1
+      if n_dbl==1 or not tags:
+        tags=sorted(dbl.keys())
+        fout.write(('\t'.join(['%s_id'%resource]+tags))+'\n')
+      vals=[]
+      for tag in tags:
+        vals.append((dbl[tag]) if tag in dbl else '')
+      fout.write(('\t'.join([id_query]+vals))+'\n')
+      n_out+=1
+
+  logging.info('n_dbl: %d'%n_dbl)
+  logging.info('n_out: %d'%n_out)
+
+#############################################################################
