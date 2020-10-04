4e7fb35a6eef41af58bda145ca6917d254a1c628
Jeremy Yang <jeremyjyang@gmail.com>
2020-10-04 15:09:05 -0600

    Ok.

--- BioClients/util/xml/Utils.py
+++ BioClients/util/xml/Utils.py
@@ -1,65 +1,22 @@
 #!/usr/bin/env python3
-'''
+"""
 	XML utility functions.
-'''
-import sys,os,re,argparse,logging
+"""
+import sys,os,re,gzip,argparse,logging
 
-from xml.etree import ElementTree #newer
+from xml.etree import ElementTree
 from xml.parsers import expat
 
-
 #############################################################################
-def DOM_NodeText(node):
-  #logging.info('DEBUG: type(node) = %s'%str(type(node)))
-  if type(node) in (ElementTree.Element, ElementTree.ElementTree):
-    return DOM_NodeText_ET(node)
-  else:
-    return DOM_NodeText_minidom(node)
-
-#############################################################################
-def DOM_NodeText_ET(node):
-  return node.text
-
-#############################################################################
-def DOM_NodeText_minidom(node):
-  for cnode in node.childNodes:
-    #if cnode.nodeName=='#text':
-    if cnode.nodeType == cnode.TEXT_NODE:
-      return cnode.nodeValue.strip()
-  return ''
-
-#############################################################################
-def DOM_GetLeafValsByTagName(root, tag):
-  if type(root) in (ElementTree.Element, ElementTree.ElementTree):
-    return DOM_GetLeafValsByTagName_ET(root, tag)
-  else:
-    return DOM_GetLeafValsByTagName_minidom(root, tag)
-
-#############################################################################
-def DOM_GetLeafValsByTagName_ET(root, tag):
+def GetLeafValsByTagName(root, tag):
   vals=[]
   for node in root.iter(tag):
-    txt=DOM_NodeText(node)
+    txt=node.text
     if txt: vals.append(txt)
   return vals
 
 #############################################################################
-def DOM_GetLeafValsByTagName_minidom(root, tag):
-  vals=[]
-  for node in root.getElementsByTagName(tag):
-    txt=DOM_NodeText(node)
-    if txt: vals.append(txt)
-  return vals
-
-#############################################################################
-def DOM_GetAttr(root, tag, attname):
-  if type(root) in (ElementTree.Element, ElementTree.ElementTree):
-    return DOM_GetAttr_ET(root, tag, attname)
-  else:
-    return DOM_GetAttr_minidom(root, tag, attname)
-
-#############################################################################
-def DOM_GetAttr_ET(root, tag, attname):
+def GetAttr(root, tag, attname):
   vals=[]
   for node in root.iter(tag):
     if attname in node.attrib:
@@ -67,67 +24,59 @@ def DOM_GetAttr_ET(root, tag, attname):
   return vals
 
 #############################################################################
-def DOM_GetAttr_minidom(root, tag, attname):
-  vals=[]
-  for node in root.getElementsByTagName(tag):
-    if attname in node.attributes:
-      vals.append(node.attributes[attname].value)
-  return vals
-
-#############################################################################
-def DOM_GetNodeAttr(node, attname):
-  if type(node) in (ElementTree.Element, ElementTree.ElementTree):
-    return DOM_GetNodeAttr_ET(node, attname)
-  else:
-    return DOM_GetNodeAttr_minidom(node, attname)
-
-#############################################################################
-def DOM_GetNodeAttr_ET(node, attname):
+def GetNodeAttr(node, attname):
   return node.attrib[attname] if attname in node.attrib else None
 
 #############################################################################
-def DOM_GetNodeAttr_minidom(node, attname):
-  return node.attributes[attname].value if attname in node.attributes else None
-
-#############################################################################
-def XpathFind(xp, root):
-  if type(root) in (ElementTree.Element, ElementTree.ElementTree):
-    return root.findall(xp)
-  else:
-    import xpath #for minidom
-    return xpath.find(xp, root)
-
-#############################################################################
 def Describe(fin):
-  tags={}
-  et_itrabl = ElementTree.iterparse(fin, events=("start", "end")) #iterable
-  #et_itr = iter(et_itrabl) #iterator
-  #event, root = et_itr.next() #root element
-  n_elem=0; n_term=0;
-  #while True:
-  #  try:
-  #    ee = et_itr.next()
-  #  except Exception as e:
-  #    break
-  #  event, elem = ee
+  n_elem=0; n_term=0; tags={};
+  et_itrabl = ElementTree.iterparse(fin, events=("start", "end"))
   for event,elem in et_itrabl:
     n_elem+=1
-    if event == 'start':
+    if event=='start':
       if elem.tag not in tags:
-        tags[elem.tag] = 0
-      tags[elem.tag] += 1
-    logging.debug('event:%6s, elem.tag:%6s, elem.text:%6s'%(event, elem.tag, elem.text))
+        tags[elem.tag]=0
+      tags[elem.tag]+=1
+    logging.debug('event:{}, elem.tag:{}, elem.text:{}'.format(event, elem.tag, elem.text))
     for k,v in elem.attrib.items():
-      logging.debug('\telem.attrib["%s"]: %s'%(k, v))
+      logging.debug('elem.attrib["{}"]: {}'.format(k, v))
     n_term+=1
-  logging.debug('n_elem: %d'%n_elem)
-  logging.debug('n_term: %d'%n_term)
+  logging.debug('n_elem: {}; n_term: {}'.format(n_elem, n_term))
   for tag in sorted(tags.keys()):
-    logging.info('%24s: %6d'%(tag, tags[tag]))
+    logging.info('{}: {}'.format(tag, tags[tag]))
 
 #############################################################################
-def XML2TSV(entity_tag, fin, fout):
-  return None
+def XML2TSV(xpath, fin, fout):
+  n_entity=0; tags=[];
+  tree = ElementTree.parse(fin)
+  root = tree.getroot()
+  logging.debug('root.tag <{}>'.format(root.tag))
+  itr = root.iter(xpath) 
+  for elem in itr:
+    logging.debug('elem.tag <{}>'.format(elem.tag))
+    n_entity+=1
+    vals={};
+    if not tags:
+      tags = [re.sub(r'^{.*}', '', se.tag) for se in list(elem)]
+      tags = sorted(tags)
+      fout.write("\t".join(tags)+"\n")
+    for subelem in list(elem):
+      val = subelem.text.strip() if subelem.text else ""
+      logging.debug('subelem.tag <{}>; text=\"{}\"'.format(subelem.tag, val))
+      vals[re.sub(r'^{.*}', '', subelem.tag)] = val
+    fout.write("\t".join([vals[tag] if tag in vals else "" for tag in tags])+"\n")
+  logging.info('n_entity <{}>: {}'.format(xpath, n_entity))
+
+#############################################################################
+def MatchXPath(xp, fin):
+  tree = ElementTree.parse(fin)
+  root = tree.getroot()
+  nodes = root.findall(xp)
+  fout.write("xpath\tmatch\n")
+  i_node=0;
+  for node in nodes:
+    fout.write("%s\t%s\n"%(xp, node.text))
+  logging.info("matches: {}".format(i_node))
 
 #############################################################################
 if __name__=="__main__":
@@ -136,7 +85,6 @@ if __name__=="__main__":
   parser.add_argument("op", choices=ops, help="operation")
   parser.add_argument("--i", dest="ifile", help="input XML file")
   parser.add_argument("--xpath", help="xpath pattern")
-  parser.add_argument("--xml2tsv_entity_tag", help="tag of entity to be enumerated")
   parser.add_argument("--force", type=bool, help="ignore UTF-8 encoding errors")
   parser.add_argument("--o", dest="ofile", help="output XML file")
   parser.add_argument("-v", "--verbose", action="count", default=0)
@@ -146,38 +94,33 @@ if __name__=="__main__":
 
   codecs_mode = "ignore" if args.force else "replace"
 
-  fin = open(args.ifile, "r") if args.ifile else sys.stdin
+  if args.ifile:
+    if re.match(r'^.*\.gz$', args.ifile):
+      fin = gzip.open(args.ifile)
+    else:
+      fin = open(args.ifile, "r")
+  else:
+    fin = sys.stdin
+
   fout = open(args.ofile, "w") if args.ofile else sys.stdout
 
   if args.op == "describe":
     Describe(fin)
 
   elif args.op == "clean":
-    try:
-      root = ElementTree.parse(fin)
-    except Exception as e:
-      parser.error("ElementTree.parse(): %s"%str(e))
+    root = ElementTree.parse(fin)
     fin.close()
     root.write(fout, encoding="UTF-8", xml_declaration=True)
 
   elif args.op == "match_xpath":
-    try:
-      root = ElementTree.parse(fin)
-    except Exception as e:
-      parser.error("ElementTree.parse(): %s"%str(e))
-    fin.close()
-    if not (xp and ifile):
-      parser.error("--i and --xpath required.")
-    nodes = XpathFind(xp, root)
-    fout.write("xpath\tmatch\n")
-    for i,node in enumerate(nodes):
-      fout.write("%s\t%s\n"%(xp, DOM_NodeText(node)))
-    logging.info("matches: %d"%len(nodes))
+    if not (args.xpath and args.ifile):
+      parser.error("--xpath required.")
+    MatchXPath(args.xpath, fin) 
 
   elif args.op == "xml2tsv":
-    if not args.xml2tsv_entity_tag:
-      parser.error("--xml2tsv_entity_tag required.")
-    XML2TSV(args.xml2tsv_entity_tag, fin, fout)
+    if not args.xpath:
+      parser.error("--xpath required.")
+    XML2TSV(args.xpath, fin, fout)
 
   else:
     parser.error("Invalid operation. %s"%(args.op))
