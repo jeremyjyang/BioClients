f4aa19b802469d0a0d026f889ace14c346cb5022
Jeremy Yang <jeremyjyang@gmail.com>
2020-11-16 16:01:13 -0700

    Ok.

--- BioClients/gwascatalog/Utils.py
+++ BioClients/gwascatalog/Utils.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+https://www.ebi.ac.uk/gwas/rest/docs/api
+"""
 ###
 import sys,os,re,json,time,logging,tqdm
 import urllib.parse
@@ -80,10 +83,10 @@ def SearchStudies(ids, searchtype, base_url=BASE_URL, fout=None):
 ##############################################################################
 def GetStudyAssociations(ids, base_url=BASE_URL, fout=None):
   """
-	Mapped genes via SNP links.
-	arg = authorReportedGene
-	sra = strongestRiskAllele
-	https://www.ebi.ac.uk/gwas/rest/api/studies/GCST001430/associations?projection=associationByStudy
+Mapped genes via SNP links.
+arg = authorReportedGene
+sra = strongestRiskAllele
+https://www.ebi.ac.uk/gwas/rest/api/studies/GCST001430/associations?projection=associationByStudy
   """
   n_id=0; n_assn=0; n_loci=0; n_arg=0; n_sra=0; n_snp=0; gcsts=set([])
   tags_assn=[]; tags_study=[]; tags_locus=[]; tags_sra=[]; tags_arg=[];
@@ -93,8 +96,7 @@ def GetStudyAssociations(ids, base_url=BASE_URL, fout=None):
     n_id+=1
     url_this = url+f'/{id_this}/associations?projection=associationByStudy'
     rval = rest.Utils.GetURL(url_this, parse_json=True)
-    if not rval:
-      continue
+    if not rval: continue
     if '_embedded' in rval and 'associations' in rval['_embedded']:
       assns = rval['_embedded']['associations']
     else:
@@ -115,60 +117,46 @@ def GetStudyAssociations(ids, base_url=BASE_URL, fout=None):
         for key in assn['loci'][0]['strongestRiskAlleles'][0].keys():
           if key != '_links':
             tags_sra.append(key)
-        fout.write('\t'.join(list(map(lambda s: 'study_'+s, tags_study))
-		+tags_assn
-		+list(map(lambda s: 'locus_'+s, tags_locus))
-		+list(map(lambda s: 'allele_'+s, tags_sra))
-		+['snp_url'])+'\n')
-      vals=[];
-      df_assn = pd.concat([df, pd.DataFrame({tags_assn[j]:[assn[tags_assn[j]]] for j in range(len(tags_assn))})])
-      df_study = pd.concat([df, pd.DataFrame({tags_study[j]:[assn['study'][tags_study[j]]] for j in range(len(tags_study))})])
-      for tag in tags_study:
-        vals.append(CleanStr(assn['study'][tag]) if tag in assn['study'] and assn['study'][tag] is not None else '')
-        try: gcsts.add(assn['study']['accessionId'])
-        except: pass
-      for tag in tags_assn:
-        vals.append(CleanStr(assn[tag]) if tag in assn and assn[tag] is not None else '')
+      df_assn = pd.DataFrame({tags_assn[j]:[assn[tags_assn[j]]] for j in range(len(tags_assn))})
+      df_study = pd.DataFrame({tags_study[j]:[assn['study'][tags_study[j]]] for j in range(len(tags_study))})
+      df_locus = pd.DataFrame({tags_locus[j]:[assn['loci'][0][tags_locus[j]]] for j in range(len(tags_locus))})
+      df_locus.columns = ['locus_'+s for s in df_locus.columns]
+      df_sra = pd.DataFrame({tags_sra[j]:[assn['loci'][0]['strongestRiskAlleles'][0][tags_sra[j]]] for j in range(len(tags_sra))})
+      df_sra.columns = ['allele_'+s for s in df_sra.columns]
+      df_this = pd.concat([df_assn,df_study, df_locus, df_sra], axis=1)
+      df = pd.concat([df, df_this], axis=0)
+      if 'accessionId' in assn['study']: gcsts.add(assn['study']['accessionId'])
+      n_loci += len(assn['loci'])
       for locus in assn['loci']:
-        n_loci+=1
-        vals_locus=[];
-        for tag in tags_locus:
-          vals_locus.append(CleanStr(locus[tag]) if tag in locus and locus[tag] is not None else '')
+        n_sra += len(locus['strongestRiskAlleles'])
         for sra in locus['strongestRiskAlleles']:
-          n_sra+=1
-          vals_sra=[]; snp_href='';
           snp_href = sra['_links']['snp']['href'] if '_links' in sra and 'snp' in sra['_links'] and 'href' in sra['_links']['snp'] else ''
           if snp_href: n_snp+=1
-          for tag in tags_sra:
-            vals_sra.append(CleanStr(sra[tag]) if tag in sra and sra[tag] is not None else '')
-          fout.write('\t'.join(vals+vals_locus+vals_sra+[snp_href])+'\n')
-
   n_gcst = len(gcsts)
   logging.info(f'INPUT RCSTs: {n_id}; OUTPUT RCSTs: {n_gcst} ; assns: {n_assn} ; loci: {n_loci} ; alleles: {n_sra} ; snps: {n_snp}')
+  if fout: df.to_csv(fout, "\t", index=False)
+  return(df)
 
 ##############################################################################
 def GetSnps(ids, base_url=BASE_URL, fout=None):
   """
-	loc = location
-	gc = genomicContext
+Input: rs_id, e.g. rs7329174
+loc = location
+gc = genomicContext
   """
   n_snp=0; n_gc=0; n_gene=0; n_gcloc=0; n_loc=0;
-  tags=[]; tags_loc=[]; tags_gc=[]; tags_gcloc=[];  tags_gene=[]; 
+  tags_snp=[]; tags_loc=[]; tags_gc=[]; tags_gcloc=[];  tags_gene=[]; 
   df=pd.DataFrame(); tq=None;
-  url=base_url+'/singleNucleotidePolymorphisms'
+  url = base_url+'/singleNucleotidePolymorphisms'
   for id_this in ids:
     n_snp+=1
-    url_this=url+'/'+id_this
-    snp=rest.Utils.GetURL(url_this, parse_json=True)
-    if not snp:
-      continue
+    url_this = url+'/'+id_this
+    snp = rest.Utils.GetURL(url_this, parse_json=True)
+    if not snp: continue
     if n_snp==1:
       for key,val in snp.items():
         if type(val) not in (list, dict):
-          tags.append(key)
-      #for key in snp['locations'][0].keys():
-      #  if key != '_links':
-      #    tags_loc.append(key)
+          tags_snp.append(key)
       for key in snp['genomicContexts'][0].keys():
         if key not in ('gene', 'location', '_links'):
           tags_gc.append(key)
@@ -178,46 +166,20 @@ def GetSnps(ids, base_url=BASE_URL, fout=None):
       for key in snp['genomicContexts'][0]['gene'].keys():
         if key != '_links':
           tags_gene.append(key)
-      fout.write('\t'.join(tags
-		+list(map(lambda s: 'genomicContext_'+s, tags_gc))
-		+list(map(lambda s: 'loc_'+s, tags_gcloc))
-		+list(map(lambda s: 'gene_'+s, tags_gene)))
-		+'\n')
-    vals=[];
-    for tag in tags:
-      vals.append(CleanStr(snp[tag]) if tag in snp and snp[tag] is not None else '')
+      df_snp = pd.DataFrame({tags_snp[j]:[snp[tags_snp[j]]] for j in range(len(tags_snp))})
+      df_gc = pd.DataFrame({tags_gc[j]:[snp['genomicContexts'][0][tags_gc[j]]] for j in range(len(tags_gc))})
+      df_gcloc = pd.DataFrame({tags_gcloc[j]:[snp['genomicContexts'][0]['location'][tags_gcloc[j]]] for j in range(len(tags_gcloc))})
+      df_gene = pd.DataFrame({tags_gene[j]:[snp['genomicContexts'][0]['gene'][tags_gene[j]]] for j in range(len(tags_gene))})
+      df_this = pd.concat([df_snp, df_gc, df_gcloc, df_gene], axis=1)
+      df = pd.concat([df, df_this], axis=0)
     for gc in snp['genomicContexts']:
       n_gc+=1
-      vals_gc=[];
-      for tag in tags_gc:
-        vals_gc.append(CleanStr(gc[tag]) if tag in gc and gc[tag] is not None else '')
       gcloc = gc['location']
       n_gcloc+=1
-      vals_gcloc=[];
-      for tag in tags_gcloc:
-        if tag=='region':
-          vals_gcloc.append(gcloc[tag]['name'] if tag in gcloc and gcloc[tag] and 'name' in gcloc[tag] and gcloc[tag]['name'] is not None else '')
-        else:
-          vals_gcloc.append(CleanStr(gcloc[tag]) if tag in gcloc and gcloc[tag] is not None else '')
       gene = gc['gene']
       n_gene+=1
-      vals_gene=[];
-      for tag in tags_gene:
-        if tag=='entrezGeneIds':
-          geneids = [g['entrezGeneId'] for g in gene[tag]]
-          if None in geneids: geneids.remove(None)
-          vals_gene.append(';'.join(geneids) if geneids else '')
-        elif tag=='ensemblGeneIds':
-          geneids = [g['ensemblGeneId'] for g in gene[tag]]
-          if None in geneids: geneids.remove(None)
-          vals_gene.append(';'.join(geneids) if geneids else '')
-        else:
-          vals_gene.append(str(gene[tag]) if tag in gene and gene[tag] is not None else '')
-      fout.write('\t'.join(vals+vals_gc+vals_gcloc+vals_gene)+'\n')
   logging.info(f'SNPs: {n_snp}; genomicContexts: {n_gc}; genes: {n_gene}; locations: {n_gcloc}')
+  if fout: df.to_csv(fout, "\t", index=False)
+  return(df)
 
 ##############################################################################
-def CleanStr(val):
-  val = re.sub(r'[\t\r\n]', ' ', str(val))
-  return val.strip()
-
