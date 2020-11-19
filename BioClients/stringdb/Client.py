29a3652e82b487d66e6c532261f1c8975982f4ba
Jeremy Yang <jeremyjyang@gmail.com>
2020-11-18 17:33:03 -0700

    Ok.

--- BioClients/stringdb/Client.py
+++ BioClients/stringdb/Client.py
@@ -17,123 +17,11 @@ stitch.embl.de
 import sys,os,re,json,argparse,time,logging
 import urllib,urllib.request,urllib.parse
 #
-from ..util import rest
+from .. import stringdb
 #
-API_HOST='string-db.org'
-API_BASE_PATH='/api'
-#
-NETWORK_FLAVORS=['evidence','confidence','actions']
-IMG_FMTS=['image','highres_image','svg']
-#
-##############################################################################
-def GetIds(base_url, pids, fout):
-  url=base_url+'/tsv/get_string_ids'
-  if len(pids)==1:
-    url+=('?identifier=%s'%pids[0])
-  else:
-    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
-  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
-  fout.write(rval)
-  logging.info('queries: %d ; results: %d'%(len(pids), len(rval.splitlines())-1))
-
-##############################################################################
-def GetInteractionPartners(base_url, pids, species, minscore, fout):
-  url=base_url+'/tsv/interaction_partners'
-  if len(pids)==1:
-    url+=('?identifier=%s'%pids[0])
-  else:
-    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
-  url+=('&species=%s'%species if species else '')
-  url+=('&required_score=%d'%minscore)
-  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
-  fout.write(rval)
-  logging.info('queries: %d ; interaction partners: %d'%(len(pids), len(rval.splitlines())-1))
-
-##############################################################################
-def GetEnrichment(base_url, pids, species, minscore, fout):
-  url=base_url+'/tsv/enrichment'
-  url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
-  url+=('&species=%s'%species if species else '')
-  url+=('&required_score=%d'%minscore)
-  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
-  fout.write(rval)
-  logging.info('query genes: %d ; enrichment results: %d'%(len(pids), len(rval.splitlines())-1))
-
-##############################################################################
-def GetPPIEnrichment(base_url, pids, species, minscore, fout):
-  url=base_url+'/tsv/ppi_enrichment'
-  url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
-  url+=('&species=%s'%species if species else '')
-  url+=('&required_score=%d'%minscore)
-  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
-  fout.write(rval)
-  logging.info('query genes: %d ; enrichment results: %d'%(len(pids), len(rval.splitlines())-1))
-
-##############################################################################
-def GetNetwork(base_url, pid, species, minscore, netflavor, fout):
-  """Format: PNG image."""
-  url=base_url+'/tsv/network?identifier=%s'%(pid)
-  url+=('&species=%s'%species if species else '')
-  url+=('&required_score=%d'%minscore)
-  url+=('&network_flavor=%s'%netflavor)
-  rval=rest.Utils.GetURL(url, parse_json=False, parse_xml=False)
-  fout.write(rval)
-  logging.info('edges: %d'%(len(rval.splitlines())-1))
-
-##############################################################################
-def GetNetworkImage(base_url, pid, species, minscore, netflavor, imgfmt, fout):
-  url=base_url+'/%s/network?identifier=%s'%(imgfmt, pid)
-  url+=('&species=%s'%species if species else '')
-  url+=('&required_score=%d'%minscore)
-  url+=('&network_flavor=%s'%netflavor)
-  rval=rest.Utils.GetURL(url, parse_json=False, parse_xml=False)
-  fout.write(rval)
-
-##############################################################################
-def GetInteractors(base_url, pids, species, minscore, fout):
-  """2018: May be deprecated since 2015."""
-  url=base_url+'/tsv/interactors'
-  if len(pids)==1:
-    url+=('?identifier=%s'%pids[0])
-  else:
-    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
-  url+=('&species=%s'%species if species else '')
-  url+=('&required_score=%d'%minscore)
-  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
-  fout.write(rval)
-  logging.info('queries: %d ; interactors: %d'%(len(pids), len(rval.splitlines())-1))
-
-##############################################################################
-def GetActions(base_url, pids, species, minscore, fout):
-  """2018: May be deprecated since 2015."""
-  url=base_url+'/tsv/actions'
-  if len(pids)==1:
-    url+=('?identifier=%s'%pids[0])
-  else:
-    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
-  url+=('&species=%s'%species if species else '')
-  url+=('&required_score=%d'%minscore)
-  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
-  fout.write(rval)
-  logging.info('queries: %d ; actions: %d'%(len(pids), len(rval.splitlines())-1))
-
-##############################################################################
-def GetAbstracts(base_url, pids, species, fout):
-  """2018: May be deprecated since 2015."""
-  url=base_url+'/tsv/abstracts'
-  if len(pids)==1:
-    url+=('?identifier=%s'%pids[0])
-  else:
-    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
-  url+=('&species=%s'%species if species else '')
-  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
-  fout.write(rval)
-  logging.info('queries: %d ; abstracts: %d'%(len(pids), len(rval.splitlines())-1))
-
 ##############################################################################
 if __name__=='__main__':
-  parser = argparse.ArgumentParser(
-        description='STRING-DB REST API client utility',
+  parser = argparse.ArgumentParser(description='STRING-DB REST API client utility',
         epilog="""\
 Example protein IDs: DRD1 DRD1_HUMAN DRD2 DRD2_HUMAN ;
 Example species: 9606 (human, via taxon identifiers, http://www.uniprot.org/taxonomy) ;
@@ -151,78 +39,73 @@ MAY BE DEPRECATED: getInteractors, getActions, getAbstracts
   parser.add_argument("--o", dest="ofile", help="output file")
   parser.add_argument("--species", help="taxon code, ex: 9606 (human)")
   parser.add_argument("--minscore", type=int, default=500, help="signifcance threshold 0-1000")
-  parser.add_argument("--netflavor", choices=NETWORK_FLAVORS, default='evidence', help="network flavor")
-  parser.add_argument("--imgfmt", choices=IMG_FMTS, default='image', help="image format")
-  parser.add_argument("--api_host", default=API_HOST)
-  parser.add_argument("--api_base_path", default=API_BASE_PATH)
+  parser.add_argument("--netflavor", choices=stringdb.Utils.NETWORK_FLAVORS, default='evidence', help="network flavor")
+  parser.add_argument("--imgfmt", choices=stringdb.Utils.IMG_FMTS, default='image', help="image format")
+  parser.add_argument("--api_host", default=stringdb.Utils.API_HOST)
+  parser.add_argument("--api_base_path", default=stringdb.Utils.API_BASE_PATH)
   parser.add_argument("-v", "--verbose", action="count", default=0)
-
   args = parser.parse_args()
 
   logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
 
   base_url='https://'+args.api_host+args.api_base_path
 
-  pids=[]
+  ids=[]
   if args.idfile:
-    fin=open(args.idfile)
-    if not fin: parser.error('failed to open input file: %s'%args.idfile)
+    fin = open(args.idfile)
     while True:
-      line=fin.readline()
+      line = fin.readline()
       if not line: break
-      pids.append(line.strip())
+      ids.append(line.strip())
   elif args.ids:
-    pids = re.split(r'\s*,\s*', args.ids.strip())
+    ids = re.split(r'\s*,\s*', args.ids.strip())
   elif args.id:
-    pids=[args.id]
+    ids=[args.id]
 
-  if args.ofile:
-    fout=open(args.ofile, "w+")
-    if not fout: parser.error('failed to open output file: %s'%args.ofile)
-  else:
-    fout=sys.stdout
+  fout = open(args.ofile, "w+") if args.ofile else sys.stdout
 
   t0=time.time()
 
   if args.op == 'getIds':
-    if not pids: parser.error('PID[s] required.')
-    GetIds(base_url, pids, fout)
+    if not ids: parser.error('PID[s] required.')
+    stringdb.Utils.GetIds(ids, base_url, fout)
 
   elif args.op == 'getInteractors':
-    if not pids: parser.error('PID[s] required.')
-    GetInteractors(base_url, pids, args.species, args.minscore, fout)
+    if not ids: parser.error('ID[s] required.')
+    stringdb.Utils.GetInteractors(ids, args.species, args.minscore, base_url, fout)
 
   elif args.op == 'getInteractionPartners':
-    if not pids: parser.error('PID[s] required.')
-    GetInteractionPartners(base_url, pids, args.species, args.minscore, fout)
+    if not ids: parser.error('ID[s] required.')
+    stringdb.Utils.GetInteractionPartners(ids, args.species, args.minscore, base_url, fout)
 
   elif args.op == 'getActions':
-    if not pids: parser.error('PID[s] required.')
-    GetActions(base_url, pids, args.species, args.minscore, fout)
+    if not ids: parser.error('ID[s] required.')
+    stringdb.Utils.GetActions(ids, args.species, args.minscore, base_url, fout)
 
   elif args.op == 'getEnrichment':
-    if not len(pids)>1: parser.error('PIDs (2+) required.')
-    GetEnrichment(base_url, pids, args.species, args.minscore, fout)
+    if not len(ids)>1: parser.error('IDs (2+) required.')
+    stringdb.Utils.GetEnrichment(ids, args.species, args.minscore, base_url, fout)
 
   elif args.op == 'getPPIEnrichment':
-    if not len(pids)>1: parser.error('PIDs (2+) required.')
-    GetPPIEnrichment(base_url, pids, args.species, args.minscore, fout)
+    if not len(ids)>1: parser.error('IDs (2+) required.')
+    stringdb.Utils.GetPPIEnrichment(ids, args.species, args.minscore, base_url, fout)
 
   elif args.op == 'getAbstracts':
-    if not pids: parser.error('PID[s] required.')
-    GetAbstracts(base_url, pids, args.species, fout)
+    if not ids: parser.error('ID[s] required.')
+    stringdb.Utils.GetAbstracts(ids, args.species, base_url, fout)
 
   elif args.op == 'getNetwork':
-    if not pids: parser.error('PID required.')
-    GetNetwork(base_url, pids[0], args.species, args.minscore, args.netflavor, fout)
+    if not ids: parser.error('ID required.')
+    stringdb.Utils.GetNetwork(ids[0], args.species, args.minscore, args.netflavor, base_url, fout)
 
   elif args.op == 'getNetworkImage':
-    if not pids: parser.error('PID required.')
+    if not ids: parser.error('ID required.')
     if not args.ofile: parser.error('--o OUTFILE required.')
     fout.close()
     fout = open(args.ofile, "wb+") #binary
-    if not fout: parser.error('failed to open output file: %s'%args.ofile)
-    GetNetworkImage(base_url, pids[0], args.species, args.minscore, args.netflavor, args.imgfmt, fout)
+    img = stringdb.Utils.GetNetworkImage(ids[0], args.species, args.minscore, args.netflavor, args.imgfmt, base_url)
+    fout.write(img)
+    fout.close()
 
   else:
     parser.print_help()
---
+++ BioClients/stringdb/Utils.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+"""
+Utility for STRING Db REST API.
+
+STRING = Search Tool for the Retrieval of Interacting Genes/Proteins
+
+See: http://string-db.org/help/api/
+
+http://[database]/[access]/[format]/[request]?[parameter]=[value]
+
+database:
+string-db.org
+string.embl.de
+stitch.embl.de
+"""
+###
+import sys,os,re,json,time,logging
+import pandas as pd
+import urllib,urllib.request,urllib.parse
+#
+from ..util import rest
+#
+API_HOST='string-db.org'
+API_BASE_PATH='/api'
+BASE_URL='https://'+API_HOST+API_BASE_PATH
+#
+NETWORK_FLAVORS=['evidence', 'confidence', 'actions']
+IMG_FMTS=['image', 'highres_image', 'svg']
+#
+##############################################################################
+def GetIds(ids, base_url=BASE_URL, fout=None):
+  tags=[]; df=pd.DataFrame();
+  for id_this in ids:
+    url_this = base_url+f'/json/get_string_ids?identifier={id_this}'
+    results = rest.Utils.GetURL(url_this, parse_json=True)
+    for result in results:
+      logging.debug(result)
+      if not tags: tags = list(result.keys())
+      df = pd.concat([df, pd.DataFrame({tags[j]:[result[tags[j]]] for j in range(len(tags))})])
+  if fout: df.to_csv(fout, "\t", index=False)
+  logging.info('queries: {} ; results: {}'.format(len(ids), df.shape[0]))
+  return df
+
+##############################################################################
+def GetInteractionPartners(ids, species, minscore, base_url=BASE_URL, fout=None):
+  tags=[]; df=pd.DataFrame();
+  for id_this in ids:
+    url_this = base_url+f'/json/interaction_partners?identifier={id_this}'
+    if species: url_this+=(f'&species={species}')
+    if minscore: url_this+=(f'&required_score={minscore}')
+    results = rest.Utils.GetURL(url_this, parse_json=True)
+    for result in results:
+      logging.debug(result)
+      if not tags: tags = list(result.keys())
+      df = pd.concat([df, pd.DataFrame({tags[j]:[result[tags[j]]] for j in range(len(tags))})])
+  if fout: df.to_csv(fout, "\t", index=False)
+  logging.info('queries: {} ; interaction partners: {}'.format(len(ids), df.shape[0]))
+  return df
+
+##############################################################################
+def GetEnrichment(ids, species, minscore, base_url=BASE_URL, fout=None):
+  tags=[]; df=pd.DataFrame();
+  url = base_url+'/json/enrichment?identifiers={}'.format(urllib.parse.quote('\n'.join(ids)))
+  if species: url+=(f'&species={species}')
+  if minscore: url+=(f'&required_score={minscore}')
+  results = rest.Utils.GetURL(url, parse_json=True)
+  for result in results:
+    logging.debug(result)
+    if not tags: tags = list(result.keys())
+    df = pd.concat([df, pd.DataFrame({tags[j]:[result[tags[j]]] for j in range(len(tags))})])
+  if fout: df.to_csv(fout, "\t", index=False)
+  logging.info('queries: {} ; enrichment results: {}'.format(len(ids), df.shape[0]))
+  return df
+
+##############################################################################
+def GetPPIEnrichment(ids, species, minscore, base_url=BASE_URL, fout=None):
+  tags=[]; df=pd.DataFrame();
+  url = base_url+'/json/ppi_enrichment?identifiers={}'.format(urllib.parse.quote('\n'.join(ids)))
+  if species: url+=(f'&species={species}')
+  if minscore: url+=(f'&required_score={minscore}')
+  results = rest.Utils.GetURL(url, parse_json=True)
+  for result in results:
+    logging.debug(result)
+    if not tags: tags = list(result.keys())
+    df = pd.concat([df, pd.DataFrame({tags[j]:[result[tags[j]]] for j in range(len(tags))})])
+  if fout: df.to_csv(fout, "\t", index=False)
+  logging.info('queries: {} ; enrichment results: {}'.format(len(ids), df.shape[0]))
+  return df
+
+##############################################################################
+def GetNetwork(nid, species, minscore, netflavor, base_url=BASE_URL, fout=None):
+  tags=[]; df=pd.DataFrame();
+  url = base_url+f'/json/network?identifier={nid}'
+  if species: url+=(f'&species={species}')
+  if minscore: url+=(f'&required_score={minscore}')
+  if netflavor: url+=(f'&network_flavor={netflavor}')
+  edges = rest.Utils.GetURL(url, parse_json=True)
+  logging.debug(json.dumps(edges, indent=2))
+  for edge in edges:
+    logging.debug(edge)
+    if not tags: tags = list(edge.keys())
+    df = pd.concat([df, pd.DataFrame({tags[j]:[edge[tags[j]]] for j in range(len(tags))})])
+  if fout: df.to_csv(fout, "\t", index=False)
+  logging.info('edges: {}'.format(df.shape[0]))
+  return df
+
+##############################################################################
+def GetNetworkImage(nid, species, minscore, netflavor, imgfmt, base_url=BASE_URL):
+  url = base_url+f'/{imgfmt}/network?identifier={nid}'
+  if species: url+=(f'&species={species}')
+  if minscore: url+=(f'&required_score={minscore}')
+  if netflavor: url+=(f'&network_flavor={netflavor}')
+  img = rest.Utils.GetURL(url, parse_json=False, parse_xml=False)
+  return img
+
+##############################################################################
+def GetInteractors(pids, species, minscore, base_url=BASE_URL, fout=None):
+  """2018: May be deprecated since 2015."""
+  tags=[]; df=pd.DataFrame();
+  url=base_url+'/tsv/interactors'
+  if len(pids)==1:
+    url+=('?identifier=%s'%pids[0])
+  else:
+    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
+  url+=('&species=%s'%species if species else '')
+  url+=('&required_score=%d'%minscore)
+  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
+  fout.write(rval)
+  logging.info('queries: %d ; interactors: %d'%(len(pids), len(rval.splitlines())-1))
+
+##############################################################################
+def GetActions(pids, species, minscore, base_url=BASE_URL, fout=None):
+  """2018: May be deprecated since 2015."""
+  tags=[]; df=pd.DataFrame();
+  url=base_url+'/tsv/actions'
+  if len(pids)==1:
+    url+=('?identifier=%s'%pids[0])
+  else:
+    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
+  url+=('&species=%s'%species if species else '')
+  url+=('&required_score=%d'%minscore)
+  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
+  fout.write(rval)
+  logging.info('queries: %d ; actions: %d'%(len(pids), len(rval.splitlines())-1))
+
+##############################################################################
+def GetAbstracts(pids, species, base_url=BASE_URL, fout=None):
+  """2018: May be deprecated since 2015."""
+  tags=[]; df=pd.DataFrame();
+  url=base_url+'/tsv/abstracts'
+  if len(pids)==1:
+    url+=('?identifier=%s'%pids[0])
+  else:
+    url+=('?identifiers=%s'%urllib.parse.quote('\n'.join(pids)))
+  url+=('&species=%s'%species if species else '')
+  rval=rest.Utils.GetURL(url, parse_xml=False, parse_json=False)
+  fout.write(rval)
+  logging.info('queries: %d ; abstracts: %d'%(len(pids), len(rval.splitlines())-1))
+
+##############################################################################
