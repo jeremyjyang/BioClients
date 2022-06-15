535a10e62a54d2f920263cf4f3de4e64dcec83ac
Jeremy Yang <jeremyjyang@gmail.com>
2022-06-15 15:15:55 -0600

    Alright.

--- BioClients/emblebi/unichem/Client.py
+++ BioClients/emblebi/unichem/Client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 EMBL-EBI Unichem
-https://chembl.gitbook.io/unichem/webservices
+UCI is UniChem Compound Id
 """
 ### 
 import sys,os,re,json,argparse,time,logging
@@ -11,17 +11,19 @@ from ... import emblebi
 #############################################################################
 if __name__=='__main__':
   epilog='''\
-
+Example InChIkey: LCNDUGHNYMJGIW-UHFFFAOYSA-N
 '''
+  INCHI_REPRESENTATIONS = ["uci", "inchi", "inchikey", "sourceID"]
   parser = argparse.ArgumentParser(description='EMBL-EBI Unichem client', epilog=epilog)
-  ops = ['getFromSourceId', 'listSources', 'getFromInchi', 'getFromInchikey'
-	]
+  ops = ['getFromSourceId', 'listSources', 'getFromInchi']
   parser.add_argument("op", choices=ops, help='OPERATION')
   parser.add_argument("--i", dest="ifile", help="Input IDs")
   parser.add_argument("--o", dest="ofile", help="Output (TSV)")
   parser.add_argument("--ids", help="Input IDs (comma-separated)")
-  parser.add_argument("--src_id_in", help="")
-  parser.add_argument("--src_id_out", help="")
+  parser.add_argument("--inchi_representation", choices=INCHI_REPRESENTATIONS, default="inchi", help=(f"[{'|'.join(INCHI_REPRESENTATIONS)}]"))
+  parser.add_argument("--src_id_in", type=int, help="")
+  parser.add_argument("--src_id_out", type=int, help="")
+  parser.add_argument("--search_components", action="store_true", help="InChI search option")
   parser.add_argument("--api_host", default=emblebi.unichem.API_HOST)
   parser.add_argument("--api_base_path", default=emblebi.unichem.API_BASE_PATH)
   parser.add_argument("-v", "--verbose", default=0, action="count")
@@ -51,10 +53,7 @@ if __name__=='__main__':
     emblebi.unichem.Utils.GetFromSourceId(ids, args.src_id_in, args.src_id_out, api_base_url, fout)
 
   elif args.op == "getFromInchi":
-    parser.error(f"Unimplemented: {args.op}")
-
-  elif args.op == "getFromInchikey":
-    parser.error(f"Unimplemented: {args.op}")
+    emblebi.unichem.Utils.GetFromInchi(ids, args.inchi_representation, args.search_components, args.src_id_in, args.src_id_out, api_base_url, fout)
 
   elif args.op == "listSources":
     emblebi.unichem.Utils.ListSources(api_base_url, fout)
--- BioClients/emblebi/unichem/Utils.py
+++ BioClients/emblebi/unichem/Utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 EMBL-EBI Unichem
+New API: https://www.ebi.ac.uk/unichem/api/docs
 https://chembl.gitbook.io/unichem/webservices
 """
 import sys,os,re,time,json,logging,requests,tqdm
@@ -8,53 +9,67 @@ import urllib,urllib.parse
 import pandas as pd
 #
 API_HOST='www.ebi.ac.uk'
-API_BASE_PATH='/unichem/rest'
+API_BASE_PATH='/unichem/api/v1'
 API_BASE_URL='https://'+API_HOST+API_BASE_PATH
 #
+HEADERS = {"Accept":"application/json", "Content-Type":"application/json"}
 #
 ##############################################################################
 def GetFromSourceId(ids, src_id_in, src_id_out=None, base_url=API_BASE_URL, fout=None):
-  n_out=0; tags=None; df=None;
+  n_out=0; df=None;
+  compound_tags=None; source_tags=None;
+  query = {"sourceID":src_id_in, "type":"sourceID"}
   for id_this in ids:
-    response = requests.get(f"{base_url}/src_compound_id/{id_this}/{src_id_in}/{src_id_out if src_id_out else ''}")
+    query["compound"] = id_this
+    response = requests.post(f"{base_url}/compounds", data=json.dumps(query), headers=HEADERS)
     if response.status_code!=200:
       logging.debug(f"Status code: {response.status_code}")
       continue
     logging.debug(response.text)
-    things = response.json()
-    for thing in things:
-      if not tags:
-        tags = list(thing.keys())
-        for tag in tags[:]:
-          if type(thing[tag]) in (list, dict):
+    result = response.json()
+    logging.debug(json.dumps(result, indent=2))
+    for compound in result["compounds"]:
+      logging.debug(json.dumps(compound, indent=2))
+      if not compound_tags:
+        compound_tags = list(compound.keys())
+        for tag in compound_tags[:]:
+          if type(compound[tag]) in (list, dict):
             logging.info(f"Ignoring field: {tag}")
-            tags.remove(tag)
-      df_this = pd.DataFrame({'src_id_in':[src_id_in], 'src_compound_id_in':[id_this]})
-      df_this = pd.concat([df_this, pd.DataFrame({tag:[thing[tag]] for tag in tags})], axis=1)
-      if fout is None: df = pd.concat([df, df_this])
-      else: df_this.to_csv(fout, "\t", index=False, header=bool(n_out==0))
-      n_out += df_this.shape[0]
+            compound_tags.remove(tag)
+      for source in compound["sources"]:
+        logging.debug(json.dumps(source, indent=2))
+        if not source_tags:
+          source_tags = list(source.keys())
+          for tag in source_tags[:]:
+            if type(source[tag]) in (list, dict):
+              logging.info(f"Ignoring field: {tag}")
+              source_tags.remove(tag)
+        if src_id_out is not None:
+          if source["id"] != src_id_out:
+            continue
+        df_this = pd.DataFrame({'src_id_in':[src_id_in], 'src_compound_id_in':[id_this]})
+        df_this = pd.concat([df_this, pd.DataFrame({f"compound_{tag}":[compound[tag]] for tag in compound_tags})], axis=1)
+        df_this = pd.concat([df_this, pd.DataFrame({f"source_{tag}":[source[tag]] for tag in source_tags})], axis=1)
+        if fout is None: df = pd.concat([df, df_this])
+        else: df_this.to_csv(fout, "\t", index=False, header=bool(n_out==0))
+        n_out += df_this.shape[0]
   logging.info(f"n_out: {n_out}")
   return df
 
 ##############################################################################
 def ListSources(base_url=API_BASE_URL, fout=None):
   n_out=0; tags=None; df=None;
-  response = requests.get(f"{base_url}/src_ids")
+  response = requests.get(f"{base_url}/sources")
   logging.debug(response.text)
-  things = response.json()
-  src_ids = [thing['src_id'] for thing in things]
-  for src_id in src_ids:
-    response = requests.get(f"{base_url}/sources/{src_id}")
-    logging.debug(response.text)
-    thing = response.json()[0]
+  result = response.json()
+  for source in result['sources']:
     if not tags:
-      tags = list(thing.keys())
+      tags = list(source.keys())
       for tag in tags[:]:
-        if type(thing[tag]) in (list, dict):
+        if type(source[tag]) in (list, dict):
           logging.info(f"Ignoring field: {tag}")
           tags.remove(tag)
-    df_this = pd.DataFrame({tag:[thing[tag]] for tag in tags})
+    df_this = pd.DataFrame({tag:[source[tag]] for tag in tags})
     if fout is None: df = pd.concat([df, df_this])
     else: df_this.to_csv(fout, "\t", index=False, header=bool(n_out==0))
     n_out += df_this.shape[0]
@@ -62,3 +77,43 @@ def ListSources(base_url=API_BASE_URL, fout=None):
   return df
 
 #############################################################################
+def GetFromInchi(ids, inchi_rep, search_components=False, src_id_in=None, src_id_out=None, base_url=API_BASE_URL, fout=None):
+  n_out=0; df=None;
+  compound_tags=None; source_tags=None;
+  query = {"searchComponents":search_components, "type":inchi_rep}
+  if src_id_in is not None: query["sourceID"] = src_id_in
+  for id_this in ids:
+    query["compound"] = id_this
+    response = requests.post(f"{base_url}/connectivity", data=json.dumps(query), headers=HEADERS)
+    if response.status_code!=200:
+      logging.debug(f"Status code: {response.status_code}")
+      continue
+    logging.debug(response.text)
+    result = response.json()
+    logging.debug(json.dumps(result, indent=2))
+    compound = result["searchedCompound"]
+    if not compound_tags:
+      compound_tags = list(compound.keys())
+      for tag in compound_tags[:]:
+        if type(compound[tag]) in (list, dict):
+          logging.info(f"Ignoring field: {tag}")
+          compound_tags.remove(tag)
+    for source in result["sources"]:
+      logging.debug(json.dumps(source, indent=2))
+      if not source_tags:
+        source_tags = list(source.keys())
+        for tag in source_tags[:]:
+          if type(source[tag]) in (list, dict):
+            logging.info(f"Ignoring field: {tag}")
+            source_tags.remove(tag)
+      if src_id_out is not None:
+        if source["id"] != src_id_out:
+          continue
+      df_this = pd.DataFrame({f"searchedCompound_{tag}":[compound[tag]] for tag in compound_tags})
+      df_this = pd.concat([df_this, pd.DataFrame({f"source_{tag}":[source[tag]] for tag in source_tags})], axis=1)
+      if fout is None: df = pd.concat([df, df_this])
+      else: df_this.to_csv(fout, "\t", index=False, header=bool(n_out==0))
+      n_out += df_this.shape[0]
+  logging.info(f"n_out: {n_out}")
+  return df
+
