bb8269f9bf86c6eac21bff1360e424160f8b8730
Jeremy Yang <jeremyjyang@gmail.com>
2022-11-17 15:09:26 -0700

    Alright.

--- BioClients/icite/Utils.py
+++ BioClients/icite/Utils.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 ###
-import sys,os,json,re,logging,tqdm
+import sys,os,json,re,logging,tqdm,requests,urllib.parse
 import pandas as pd
-#
-from ..util import rest
 #
 API_HOST="icite.od.nih.gov"
 API_BASE_PATH="/api/pubs"
@@ -23,11 +21,14 @@ cited_by) reported as counts."""
     pmids_this = pmids[n_in:n_in+NCHUNK]
     n_in += (NCHUNK if n_in+NCHUNK < len(pmids) else len(pmids)-n_in)
     url_this = (f"""{base_url}?pmids={(','.join(pmids_this))}""")
-    rval = rest.GetURL(url_this, parse_json=True)
-    if not rval: break
-    logging.debug(json.dumps(rval, indent=2))
-    url_self = rval['links']['self']
-    pubs = rval['data']
+    response = requests.get(url_this)
+    if response.status_code != 200:
+      logging.error(f"status_code: {response.status_code}")
+      break
+    result = response.json()
+    logging.debug(json.dumps(result, indent=2))
+    url_self = result['links']['self']
+    pubs = result['data']
     for pub in pubs:
       if not tags: tags = list(pub.keys())
       df_this = pd.DataFrame({tags[j]:[pub[tags[j]]] for j in range(len(tags))})
@@ -47,10 +48,11 @@ def GetStats_single(pmids, base_url=BASE_URL, fout=None):
     if tq is None: tq = tqdm.tqdm(total=len(pmids), unit="pmids")
     tq.update()
     url = base_url+'/'+pmid
-    pub = rest.GetURL(url, parse_json=True)
-    if not pub:
-      logging.info(f"Not found: {pmid}")
+    response = requests.get(url)
+    if response.status_code != 200:
+      logging.error(f"status_code: {response.status_code}")
       continue
+    pub = response.json()
     if not tags: tags = list(pub.keys())
     df = pd.concat([df, pd.DataFrame({tags[j]:[pub[tags[j]]] for j in range(len(tags))})])
   if fout: df.to_csv(fout, "\t", index=False)
