83d51989e4834d320c55308c597b9c50a849b6fd
Jeremy Yang <jeremyjyang@gmail.com>
2025-10-16 16:44:53 -0600

    Ok.

--- BioClients/umls/CPTClient.py
+++ BioClients/umls/CPTClient.py
@@ -6,13 +6,81 @@ Ref: https://www.ama-assn.org/topics/cpt-codes
 
 https://cts.nlm.nih.gov/fhir/res/CodeSystem/$lookup?system=http://www.ama-assn.org/go/cpt&version=2025&code=0031U
 
-https://cts.nlm.nih.gov/fhir/res/CodeSystem/$lookup?system=http%3A%2F%2Fwww.ama-assn.org%2Fgo%2Fcpt&version=2025&_format=json&code=0031U
+https://cts.nlm.nih.gov/fhir/res/CodeSystem/$lookup?system=http%3A%2F%2Fwww.ama-assn.org%2Fgo%2Fcpt&version=2025&code=0031U
+
+https://cts.nlm.nih.gov/fhir/res/CodeSystem/$lookup?system=http://www.ama-assn.org/go/cpt&version=2025&code=0113U
 """
 ###
-import sys,os,argparse,re,yaml,json,csv,logging,requests,time
+import sys,os,argparse,re,yaml,json,logging,requests,urllib,time
 #
 from .. import umls
 #
+API_HOST="cts.nlm.nih.gov"
+API_BASE_PATH="/fhir/res/CodeSystem"
+API_CODE_SYSTEM="http://www.ama-assn.org/go/cpt"
+API_CODE_VERSION="2025"
+#
+#############################################################################
+def CPTLogin(api_key):
+  url_this = "https://cts.nlm.nih.gov/fhir/login"
+  headers = {
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          'Accept-Language': 'en-US,en;q=0.5',
+          'Accept-Encoding': 'gzip, deflate, br, zstd',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          #'Origin': 'https://cts.nlm.nih.gov',
+          'Connection': 'keep-alive',
+          'Referer': 'https://cts.nlm.nih.gov/fhir/login.html'
+          }
+  data = {
+          'username':'apikey',
+          'password':api_key
+          }
+  response = requests.post(url_this, headers=headers, data=data)
+  if (response.status_code!=200):
+    logging.error(f"(status_code={response.status_code};{response.reason};{response.text}): url_this: {url_this}")
+    return
+  else:
+    logging.debug(f"(status_code={response.status_code};{response.reason};{response.text}): url_this: {url_this}")
+
+  rval = response.json()
+  if rval:
+    logging.debug(json.dumps(rval, sort_keys=True, indent=2))
+
+  return
+
+#############################################################################
+def GetCPTCodes(ids, api_key, base_url, fout=None):
+  """
+  https://cts.nlm.nih.gov/fhir/res/CodeSystem/$lookup?system=http://www.ama-assn.org/go/cpt&version=2025&code=0113U
+
+  How to include api key in header?
+"""
+
+
+  headers = {
+          "Accept": "application/json"
+          }
+  n_out=0; df=None;
+  for id_this in ids:
+    url_this = f"{base_url}&code={id_this}"
+    response = requests.get(url_this, headers=headers)
+    if (response.status_code!=200):
+      logging.error(f"(status_code={response.status_code};{response.reason};{response.text}): url_this: {url_this}")
+      continue
+    rval = response.json()
+    if not rval: continue
+    logging.debug(json.dumps(rval, sort_keys=True, indent=2))
+
+
+    df_this = pd.DataFrame({'CPT_code':[id_this], 'name': 'TBA'})
+    if fout is None: df = pd.concat([df, df_this])
+    else: df_this.to_csv(fout, sep="\t", index=False, header=bool(n_out==0))
+    n_out+=1
+  logging.info(f'n_id: {len(ids)}')
+  logging.info(f'n_out: {n_out}')
+  if fout is None: return df
+
 #############################################################################
 if __name__=='__main__':
   EPILOG = f"""\
@@ -20,7 +88,7 @@ From the AMA website, https://www.ama-assn.org/topics/cpt-codes:
 "Current Procedural Terminology (CPT) codes provide a uniform nomenclature for coding medical procedures and services. Medical CPT codes are critical to streamlining reporting and increasing accuracy and efficiency, as well as for administrative purposes such as claims processing and developing guidelines for medical care review. The AMA develops and manages CPT codes on a rigorous and transparent process led by the CPT Editorial Panel, which ensures codes are issued and updated regularly to reflect current clinical practice and innovation in medicine."
 """
   parser = argparse.ArgumentParser(description='CPT Client, an UMLS REST API client utility', epilog=EPILOG)
-  ops = ['getCodes', 'search']
+  ops = ['getCodes', 'login']
   parser.add_argument("op", choices=ops, help='OPERATION (select one)')
   parser.add_argument("--ids", help="CPT code IDs (comma-separated) (ex:0153U,81441)")
   parser.add_argument("--idfile", help="input CPT code IDs")
@@ -28,12 +96,11 @@ From the AMA website, https://www.ama-assn.org/topics/cpt-codes:
   parser.add_argument("--searchQuery", help='string or code')
   parser.add_argument("--skip", default=0, type=int)
   parser.add_argument("--nmax", default=0, type=int)
-  parser.add_argument("--api_version", default=f"{umls.API_VERSION}", help=f"API version {umls.API_VERSION}")
-  parser.add_argument("--api_host", default=umls.API_HOST)
-  parser.add_argument("--api_base_path", default=umls.API_BASE_PATH)
-  parser.add_argument("--api_auth_host", default=umls.API_AUTH_HOST)
-  parser.add_argument("--api_auth_endpoint", default=umls.API_AUTH_ENDPOINT)
-  parser.add_argument("--api_auth_service", default=umls.API_AUTH_SERVICE)
+  #parser.add_argument("--api_version", default=f"{API_VERSION}", help=f"API version {API_VERSION}")
+  parser.add_argument("--api_host", default=API_HOST)
+  parser.add_argument("--api_base_path", default=API_BASE_PATH)
+  parser.add_argument("--api_code_system", default=API_CODE_SYSTEM)
+  parser.add_argument("--api_code_version", default=API_CODE_VERSION)
   parser.add_argument("--param_file", default=os.environ['HOME']+"/.umls.yaml")
   parser.add_argument("--api_key", help="API key")
   parser.add_argument("-v", "--verbose", default=0, action="count")
@@ -42,7 +109,7 @@ From the AMA website, https://www.ama-assn.org/topics/cpt-codes:
 
   logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
 
-  base_url='https://'+args.api_host+args.api_base_path
+  base_url = f"https://{args.api_host}{args.api_base_path}/$lookup?system={urllib.parse.quote(args.api_code_system)}&version={args.api_code_version}"
 
   fout = open(args.ofile, "w+") if args.ofile else sys.stdout
 
@@ -50,11 +117,6 @@ From the AMA website, https://www.ama-assn.org/topics/cpt-codes:
   if args.api_key: params['API_KEY'] = args.api_key
   if not params['API_KEY']:
     parser.error('Please specify valid API_KEY via --api_key or --param_file') 
-
-  api_auth_url = 'https://'+args.api_auth_host+args.api_auth_endpoint
-  auth = umls.Authentication(params['API_KEY'],
-args.api_auth_service, api_auth_url, umls.API_HEADERS)
-  auth.setVerbosity(args.verbose)
 
   ids=[];
   if args.idfile:
@@ -71,12 +133,10 @@ args.api_auth_service, api_auth_url, umls.API_HEADERS)
   t0 = time.time()
 
   if args.op == 'getCodes':
-    umls.GetCodes(ids, args.srcs, auth, args.api_version, base_url, fout)
+    GetCPTCodes(ids, params['API_KEY'], base_url, fout)
 
-  elif args.op == 'search':
-    if not args.searchQuery:
-      parser.error('search requires --searchQuery')
-    umls.Search(args.searchQuery, args.searchType, args.inputType, args.returnIdType, args.srcs, auth, args.api_version, base_url, fout)
+  elif args.op == 'login':
+    CPTLogin(params['API_KEY'])
 
   else:
     parser.error(f'Invalid operation: {args.op}')
