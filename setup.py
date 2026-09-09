f72bf633167d526c4ee3d1a88d1cb5221e8895bf
Jeremy Yang <jeremyjyang@gmail.com>
2026-09-09 16:23:12 -0600

    Alright.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="BioClients",
-    version="0.2.32",
+    version="0.2.33",
     author="Jeremy Yang",
     author_email="jeremyjyang@gmail.com",
-    description="Clients for online biomedical resources, usually via REST APIs.",
+    description="Clients and tools for online biomedical resources, usually via REST APIs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeremyjyang/BioClients",
